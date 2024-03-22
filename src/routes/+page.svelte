da682cdc6dfd1a70cf9121ce7f35329b4d0ee000
Armand Coretchi <armand@tamarix.tech>
2024-03-22 13:18:48 +0000

    Add user survey to home page.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -65,6 +65,9 @@
             <div class="text-sm mx-auto text-gray-400 font-semibold mb-4 md:mb-8">
                 By <a href="https://armandcoretchi.com" target="_blank" class="text-blue-400 underline">Armand Coretchi</a>.
             </div>
+            <span class="text-md md:text-xl font-bold mx-auto mb-2 md:mb-6">
+                Please complete the <a href="https://forms.gle/bthpuGJ6CdeES7q9A" target="_blank" class="text-blue-400 underline">user survey</a> after using the tool!
+            </span>
 
             {#if selectedAlgorithm === null}
                 <h2 class="text-xl md:text-2xl font-bold mx-auto mb-2 md:mb-6">
